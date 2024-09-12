2849db1efe596331e616abc722e1e6800675ef7c
Vector-ops <sumith2827@gmail.com>
2024-09-12 20:43:06 +0530

    chore: setup redis connection

--- configs/redis.go
+++ configs/redis.go
@@ -1,1 +1,21 @@
 package configs
+
+import (
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRedisClient() *redis.Client {
+	redisHost := GetEnv("REDIS_HOST", "localhost")
+	redisPort := GetEnv("REDIS_PORT", "6379")
+	redisPassword := GetEnv("REDIS_PASSWORD", "")
+	addr := fmt.Sprintf("%s:%s", redisHost, redisPort)
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: redisPassword,
+		DB:       0,
+	})
+
+	return rdb
+}
