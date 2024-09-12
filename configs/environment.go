7f0fd4aa9a75c23e77edbeb2f28cb1f785080769
Vector-ops <sumith2827@gmail.com>
2024-09-12 20:42:30 +0530

    feat: add env loader function

--- configs/environment.go
+++ configs/environment.go
@@ -1,1 +1,21 @@
 package configs
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetEnv(key, fallback string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return fallback
+}
